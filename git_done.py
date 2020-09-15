215941ff8b72cd7bb48373c313d23e783ec40bce
waseem <waseemmedhatg@gmail.com>
2020-09-15 02:00:08 +0200

    initial commit

---
+++ git_done.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+from git import Repo, GitCommandError
+import sys
+
+if len(sys.argv) < 2:
+    print("No message given")
+    sys.exit()
+else:
+    commit_msg = sys.argv[1]
+
+repo = Repo(".")
+repo.git.add("--all")
+
+try:
+    repo.git.commit("-m", commit_msg)
+except GitCommandError:
+    print("Nothing to commit!")
+    sys.exit()
+
+
+print(f"added commit \"{commit_msg}\"")
+
+origin = repo.remote()
